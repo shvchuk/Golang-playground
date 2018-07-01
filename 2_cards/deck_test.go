99d958ae2b269b0223814d572a7e0d868777b082
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2018-07-02 00:40:23 +0200

    saving & reading from file tests done

--- 2_cards/deck_test.go
+++ 2_cards/deck_test.go
@@ -16,8 +16,8 @@ func TestNewDeck(t *testing.T) {
 		t.Errorf("Expected first card of Ace of Spades, but got %v", d[0])
 	}
 
-	if d[len(d) - 1] != "Four of Clubs" {
-		t.Errorf("Expected last card of Four of Clubs, but got %v", d[len(d) - 1])
+	if d[len(d)-1] != "Four of Clubs" {
+		t.Errorf("Expected last card of Four of Clubs, but got %v", d[len(d)-1])
 	}
 }
 
@@ -25,6 +25,13 @@ func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
 	os.Remove("_decktesting")
 
 	deck := newDeck()
-	d.saveToFile("_decktesting")
-	
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16 cards in deck, got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
