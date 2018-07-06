410ad37f9893c454f453aad459ab965cd227766e
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2018-07-06 23:47:01 +0200

    pointers basics

--- 3_structs/main.go
+++ 3_structs/main.go
@@ -25,12 +25,13 @@ func main() {
 		},
 	}
 
-	andrew.updateName("andreas")
+	andrewPointer := &andrew
+	andrewPointer.updateName("andreas")
 	andrew.print()
 }
 
-func (p person) updateName(newFirstName string) {
-	p.firstName = newFirstName
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
 }
 
 func (p person) print() {
