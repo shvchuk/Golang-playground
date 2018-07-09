5d19a9bf923e6f3c6743e1823cad0e06190dce4e
Andrzej Szewczuk <andrzej.szewczuk@gmail.com>
2018-07-09 10:13:36 +0200

    iteration over maps

--- 4_maps/main.go
+++ 4_maps/main.go
@@ -5,10 +5,18 @@ import (
 )
 
 func main() {
+
 	colors := map[string]string{
 		"red":   "#ff0000",
 		"green": "#4b23e",
+		"white": "#fffff",
 	}
 
-	fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
